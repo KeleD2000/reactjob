14c050c4e6f84b23c7763a3effed55dd8b90c847
KeleD <k.dominik2000@gmail.com>
2023-05-19 23:23:21 +0200

    validation done

--- src/EForm.js
+++ src/EForm.js
@@ -9,11 +9,39 @@ const EForm = () => {
     jobTitle: "",
   });
 
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    email: "",
+    age: "",
+    jobTitle: "",
+  });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
+    }));
+
+    let errorMessage = "";
+
+    if (name === "email" && value.trim() !== "") {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(value)) {
+        errorMessage = "Invalid email.";
+      }
+    }
+
+    if (name === "age" && value.trim() !== "") {
+      const ageValue = parseInt(value);
+      if (ageValue <= 0 || ageValue < 18) {
+        errorMessage = "The age must be 18 and positiv number.";
+      }
+    }
+
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: errorMessage,
     }));
   };
 
@@ -22,15 +50,41 @@ const EForm = () => {
       ...prevData,
       jobTitle: selectedJobTitle,
     }));
+
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      jobTitle: selectedJobTitle === "" ? "This field is must be fill in." : "",
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const jsonData = JSON.stringify(formData);
-    console.log(jsonData);
+
+    const updatedErrors = {
+      name: formData.name.trim() === "" ? "This field is must be fill in." : "",
+      email:
+        formData.email.trim() === ""
+          ? "This field is must be fill in."
+          : formErrors.email,
+      age: formData.age.trim() === "" ? "This field is must be fill in." : "",
+      jobTitle:
+        formData.jobTitle === "" ? "This field is must be fill in." : "",
+    };
+
+    setFormErrors(updatedErrors);
+
+    if (Object.values(updatedErrors).every((error) => error === "")) {
+      const jsonData = JSON.stringify(formData);
+      console.log(jsonData);
+    }
   };
 
-  const jobTitleOptions = ["Accountant", "Software Developer", "Software Tester", "Manager"];
+  const jobTitleOptions = [
+    "Accountant",
+    "Software Developer",
+    "Software Tester",
+    "Manager",
+  ];
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -53,9 +107,13 @@ const EForm = () => {
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
-            required
+            className={`block w-full border rounded p-1 mb-1 ${
+              formErrors.name ? "border-red-500" : "border-gray-300"
+            }`}
           />
+          {formErrors.name && (
+            <p className="error-message">{formErrors.name}</p>
+          )}
         </label>
       </div>
       <div className="input-container">
@@ -66,9 +124,13 @@ const EForm = () => {
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
-            required
+            className={`block w-full border rounded p-1 mb-1 ${
+              formErrors.email ? "border-red-500" : "border-gray-300"
+            }`}
           />
+          {formErrors.email && (
+            <p className="error-message">{formErrors.email}</p>
+          )}
         </label>
       </div>
       <div className="input-container">
@@ -79,29 +141,40 @@ const EForm = () => {
             name="age"
             value={formData.age}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
-            required
+            className={`block w-full border rounded p-1 mb-1 ${
+              formErrors.age ? "border-red-500" : "border-gray-300"
+            }`}
           />
+          {formErrors.age && (
+            <p className="error-message">{formErrors.age}</p>
+          )}
         </label>
       </div>
-      <div className={`input-container ${isDropdownOpen ? 'mb-10' : ''}`}>
+      <div className={`input-container ${isDropdownOpen ? "mb-10" : ""}`}>
         <label>
           Job title:
           <div className="relative inline-block">
             <button
               onClick={toggleDropdown}
-              className={`text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 text-center inline-flex items-center ${isDropdownOpen ? 'rounded-b-none' : ''}`}
+              className={`text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-1 text-center inline-flex items-center ${
+                isDropdownOpen ? "rounded-b-none" : ""
+              }`}
             >
-              {formData.jobTitle ? formData.jobTitle : "Select Job Title"}
+              {formData.jobTitle ? formData.jobTitle : "Select Job"}
               <svg
-                className={`w-4 h-4 ml-1 transform ${isDropdownOpen ? 'rotate-180' : 'rotate-0'}`}
+                className="w-4 h-4 ml-2"
                 aria-hidden="true"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9-7"></path>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
               </svg>
             </button>
             {isDropdownOpen && (
@@ -110,7 +183,7 @@ const EForm = () => {
                   <button
                     key={jobTitle}
                     onClick={() => selectJobTitle(jobTitle)}
-                    className="block w-full px-3 py-1 hover:bg-gray-100 text-gray-700 text-left"
+                    className="block w-full py-1 hover:bg-gray-100 text-gray-700 text-left"
                   >
                     {jobTitle}
                   </button>
@@ -118,23 +191,31 @@ const EForm = () => {
               </div>
             )}
           </div>
+          {formErrors.jobTitle && (
+            <p className="error-message">{formErrors.jobTitle}</p>
+          )}
         </label>
       </div>
       <div className="input-container">
         <label>
-          CV_field:
+          CV field:
           <input
             type="file"
             name="CV_field"
+            accept=".pdf"
             value={formData.CV_field}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
-            required
+            className="block w-full border rounded p-1 mb-1"
           />
         </label>
       </div>
       <div className="flex justify-center">
-        <button type="submit" className={`submit-button bg-blue-500 text-white px-2 py-1 rounded ${isDropdownOpen ? 'hidden' : 'block'}`}>
+        <button
+          type="submit"
+          className={`submit-button bg-blue-500 text-white px-2 py-1 rounded ${
+            isDropdownOpen ? "hidden" : "block"
+          }`}
+        >
           Submit
         </button>
       </div>
--- src/Form.js
+++ src/Form.js
@@ -9,18 +9,60 @@ const Form = () => {
     description: "",
   });
 
+  const [formErrors, setFormErrors] = useState({
+    name: "",
+    email: "",
+    numberofe: "",
+  });
+
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
     }));
+    setFormErrors((prevErrors) => ({
+      ...prevErrors,
+      [name]: value.trim() === "" ? "This field is required." : "",
+      email: name === "email" && !validateEmail(value) ? "Invalid email." : "",
+      numberofe:
+        name === "numberofe" && !validateNumberofe(value)
+          ? "The number of employees must be between 1 and 100."
+          : "",
+    }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const jsonData = JSON.stringify(formData);
-    console.log(jsonData);
+    const { name, email, numberofe} = formData;
+
+    const updatedErrors = {
+      name: name.trim() === "" ? "This field is must be fill in ." : "",
+      email: email.trim() === "" ? "This field is must be fill in." : !validateEmail(email) ? "Invalid email." : "",
+      numberofe:
+        numberofe.trim() === "" ? "This field is must be fill in." : !validateNumberofe(numberofe)
+          ? "The number of employees must be between 1 and 100."
+          : "",
+    };
+
+    setFormErrors(updatedErrors);
+
+    if (Object.values(updatedErrors).every((error) => error === "")) {
+      const jsonData = JSON.stringify(formData);
+      console.log(jsonData);
+    }
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validateNumberofe = (numberofe) => {
+    const numberofeValue = parseInt(numberofe);
+    return (
+      !isNaN(numberofeValue) && numberofeValue >= 1 && numberofeValue <= 100
+    );
   };
 
   return (
@@ -33,9 +75,14 @@ const Form = () => {
             name="name"
             value={formData.name}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
+            className={`block w-full border ${
+              formErrors.name ? "border-red-500" : "border-gray-300"
+            } rounded p-1 mb-1`}
           />
         </label>
+        {formErrors.name && (
+          <p className="error-message">{formErrors.name}</p>
+        )}
       </div>
       <div className="input-container">
         <label>
@@ -45,9 +92,14 @@ const Form = () => {
             name="email"
             value={formData.email}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
+            className={`block w-full border ${
+              formErrors.email ? "border-red-500" : "border-gray-300"
+            } rounded p-1 mb-1`}
           />
         </label>
+        {formErrors.email && (
+          <p className="error-message">{formErrors.email}</p>
+        )}
       </div>
       <div className="input-container">
         <label>
@@ -57,9 +109,14 @@ const Form = () => {
             name="numberofe"
             value={formData.numberofe}
             onChange={handleChange}
-            className="block w-full border border-gray-300 rounded p-1 mb-1"
+            className={`block w-full border ${
+              formErrors.numberofe ? "border-red-500" : "border-gray-300"
+            } rounded p-1 mb-1`}
           />
         </label>
+        {formErrors.numberofe && (
+          <p className="error-message">{formErrors.numberofe}</p>
+        )}
       </div>
       <div className="input-container">
         <label>
